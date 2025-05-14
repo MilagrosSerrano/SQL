3de2a5471a71f6f1211221ea9f34c87fb28408a4
MilagrosSerrano <serranomilagros23@gmail.com>
2025-05-14 00:11:57 -0300

    añade ejercicios en movies.sql

--- movies.sql
+++ movies.sql
@@ -83,3 +83,32 @@ last_name AS apellido,
 rating AS puntuacion
 FROM actors
 ORDER BY puntuacion DESC;
+
+
+/* Funciones de agregación, GROUP BY y HAVING
+1. ¿Cuántas películas hay?
+2. ¿Cuántas películas tienen entre 3 y 7 premios?
+3. ¿Cuántas películas tienen entre 3 y 7 premios y un rating mayor a 7?
+4. Crear un listado a partir de la tabla de películas, mostrar un reporte de la cantidad de películas por id. de género.
+5. De la consulta anterior, listar sólo aquellos géneros que tengan como suma de
+premios un número mayor a 5. */
+
+SELECT SUM(id) AS cantidad_peliculas
+FROM movies;
+
+SELECT title, awards
+FROM movies
+WHERE awards BETWEEN '3' AND '7';
+
+SELECT title, awards, rating
+FROM movies
+WHERE awards BETWEEN '3' AND '7' AND rating > 7;
+
+SELECT genre_id, COUNT(genre_id) AS cant_genre
+FROM movies
+GROUP BY genre_id;
+
+SELECT genre_id, COUNT(genre_id) AS cant_genre, SUM(awards) AS total_awards
+FROM movies
+GROUP BY genre_id
+HAVING SUM(awards) > 5;
